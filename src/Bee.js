38c92dc4e8d1d9b35a562fdf88be1947f506be3b
Daisy Ding <xd222@cornell.edu>
2022-06-21 18:19:34 -0700

    Complete Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,4 +1,9 @@
 var Bee = function() {
   Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
